df9e02e6d69e65c5f79ff955477206e06a150da3
sk-deku <kesavansivakumar50@gmail.com>
2025-03-13 17:04:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://AnimeFireBot:Skesavan7@cluster0.zu5mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB connection URL
     SHORTENER_API = os.getenv("SHORTENER_API", "5a6b57d3cbd44e9b81cda3a2ec9d93024fcc6838")  # Your shortener API key
     SHORTENER_URL = os.getenv("SHORTENER_URL", "http://modijiurl.com")  # Your shortener site URL
-    ADMINS = list(map(int, os.getenv("ADMINS", "1775977570").split()))  # List of admin user IDs
+    ADMINS = list(map(int, os.getenv("ADMINS", "1775977570").split())) if os.getenv("ADMINS") else [] # List of admin user IDs
     PAYMENT_UPI = os.getenv("PAYMENT_UPI", "sivakumar089356@oksbi")  # UPI ID for payments
     PAYMENT_QR = os.getenv("PAYMENT_QR", "https://graph.org/file/3c17f2014b6c468b108df-01888ecfbde40c16bf.jpg")  # QR code URL for payments
 
