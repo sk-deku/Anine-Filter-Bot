544c34d2218551210437caaab6051eb961b8805e
sk-deku <kesavansivakumar50@gmail.com>
2025-03-13 19:58:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Config:
     API_HASH = os.environ["API_HASH"]
     BOT_TOKEN = os.environ["BOT_TOKEN"]
     DATABASE_URL = os.environ["DATABASE_URL"]
-
+    
     # Optional variables with defaults
-    SHORTENER_API = os.getenv("SHORTENER_API", "")
-    SHORTENER_URL = os.getenv("SHORTENER_URL", "")
-    ADMINS = list(map(int, os.getenv("ADMINS", "").split())) if os.getenv("ADMINS") else []
-    PAYMENT_UPI = os.getenv("PAYMENT_UPI", "")
-    PAYMENT_QR = os.getenv("PAYMENT_QR", "")
+    SHORTENER_API = os.getenv("SHORTENER_API", "5a6b57d3cbd44e9b81cda3a2ec9d93024fcc6838")
+    SHORTENER_URL = os.getenv("SHORTENER_URL", "https://modijiurl.com/")
+    ADMINS = list(map(int, os.getenv("ADMINS", "1775977570").split())) if os.getenv("ADMINS") else []
+    PAYMENT_UPI = os.getenv("PAYMENT_UPI", "sivakumar089356@oksbi")
+    PAYMENT_QR = os.getenv("PAYMENT_QR", "https://graph.org/file/3c17f2014b6c468b108df-01888ecfbde40c16bf.jpg")
 
     TOKEN_PRICING = {
         20: 50,
